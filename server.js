376dbb4312c70c71d0463ab0f90d8226421d4a1d
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-05-04 20:26:57 -0700

    add project structure and backend

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var session = require("express-session");
+var passport = require("./config/passport");
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3001;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+// =============================================================
+// require("./routes/api-routes")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
