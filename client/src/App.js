d6faa1ac7d513f53cb173e6b48902a46b0827fa7
Shipra <drshipradyundi@gmail.com>
2020-05-07 21:04:10 -0700

    added signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Homepage from "./pages/Homepage";
+// import Projects from "./pages/Projects";
+import Signup from "./pages/Signup";
+// import Login from "./pages/Login";
+
+class App extends Component {
+
+  state = {
+    loggedIn: false
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div>
+          {/* <NavTabs updateUser={this.updateUser} loggedIn={this.state.loggedIn} /> */}
+          <Route exact path="/signup" component={Signup} />
+          {/* <Route exact path="/login" render={() =>
+            <Login
+              updateUser={this.updateUser}
+            />} />
+          <Route exact path="/" render={() =>
+            <Homepage
+              loggedIn={this.state.loggedIn}
+            />} /> */}
+          {/* <Route exact path="/projects" component={Projects} /> */}
+        </div>
+      </Router>
   );
+}
 }
 
 export default App;
