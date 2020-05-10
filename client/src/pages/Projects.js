55c17debbe2c36fa3fcc51463d2b0b43720bb4ac
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-05-09 19:37:52 -0700

    add Projects page and new project form

---
+++ client/src/pages/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Button from 'react-bootstrap/Button'
+// import API from "../utils/API";
+
+class Projects extends Component {
+    render() {
+        return (
+            <Button href="/login">Create New Project</Button>
+        )
+    }
+}
+
+export default Projects;
