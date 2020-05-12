b18c1aa2ae0f5f2b0c7520d8f4cee23407d3346c
Shipra <drshipradyundi@gmail.com>
2020-05-11 21:06:43 -0700

    started with the projects page and added project card

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,13 +1,45 @@
 import React, { Component } from "react";
 import Button from 'react-bootstrap/Button'
-// import API from "../utils/API";
-
+import API from "../utils/API";
+import ProjectCard from "../components/ProjectCard"
 class Projects extends Component {
+
+state = {
+    projects : []
+}
+
+componentDidMount = () => {
+    this.handleProjects(this.state.projects);
+  }
+
+ handleProjects = id => {
+  API.getProjects().then(res => {
+    this.setState({projects: res.data.results })
+  })
+    .catch(err => console.log(err));
+}
+
     render() {
         return (
-            <Button href="/projects/add">Create New Project</Button>
-        )
+       <div>
+        <Button href="/projects/add">Create New Project</Button>
+
+          <div className="row">
+          {this.state.projects.map(project => {
+            return (
+              <ProjectCard
+                key={project.id}
+                id={project.UserId}
+                title={project.title}
+                description={project.description}
+                
+              />)
+          })}
+        </div>
+        </div>
+        ) 
     }
+
 }
 
 export default Projects;
