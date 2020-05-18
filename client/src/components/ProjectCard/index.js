c071619821f000d376b9daa5ffd8bcf42f8f7d2b
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-05-17 18:42:41 -0700

    fix api routes for tasks page

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -5,24 +5,24 @@ import Button from 'react-bootstrap/Button';
 
 class ProjectCard extends Component {
 
-            render() {
-                return (
-                    <Card>
-                    <Card.Body>
-                   <Card.Title>{this.props.title}</Card.Title>
-                      <Card.Text>
-                        {this.props.description}
-                      </Card.Text>
-                      <Button href={"/projects/" + this.props.project_id}>Launch</Button>
-                      
-                    </Card.Body>
-                    </Card>
-                  )
-                
-                }
-        
-        
-        
-    }
+  render() {
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>
+            {this.props.description}
+          </Card.Text>
+          <Button href={"/projects/" + this.props.project_id}>Launch</Button>
+
+        </Card.Body>
+      </Card>
+    )
+
+  }
+
+
+
+}
 
 export default ProjectCard;
--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -2,27 +2,30 @@ import React, { Component } from "react";
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
- import API from "../utils/API";
+import API from "../utils/API";
 import TaskCard from "../components/TaskCard";
 
 class Tasks extends Component {
- 
+
     state = {
         tasks: []
-      }
+    }
 
-     componentDidMount = () => {
-        this.handleTasks(this.state.tasks);
-      }
-    
+    componentDidMount = () => {
+        this.handleTasks();
+    }
 
-     handleTasks = (id) =>  {
-        console.log("I'm here", id)
-        API.getTasks(id)
-        .then(res => {
-            this.setState({ tasks: res.data})
-        })
-        .catch(err => console.log(err));
+
+    handleTasks = () => {
+        console.log("I'm here")
+        console.log(this.state)
+        var projectId = window.location.pathname.split("/")[2];
+        API.getTasks(projectId)
+            .then(res => {
+                console.log(res.data)
+                this.setState({ tasks: res.data })
+            })
+            .catch(err => console.log(err));
     }
 
     style = {
@@ -39,19 +42,17 @@ class Tasks extends Component {
 
                 <Row>
                     <Col sm style={this.style}><h1>To do</h1>
-                {this.state.tasks.map(task => {
-                return (
-                <TaskCard
-                key={task.id}
-                title={task.title}
-                status={task.status}/> 
-                )
-                })}</Col>
+                        {this.state.tasks.map(task => {
+                            return (
+                                <TaskCard
+                                    key={task.id}
+                                    title={task.title}
+                                    status={task.status} />
+                            )
+                        })}</Col>
                     <Col sm><h1>In Progress</h1></Col>
                     <Col sm><h1>Done</h1></Col>
                 </Row>
-
-                <TaskCard/>
             </div>
         )
     }
